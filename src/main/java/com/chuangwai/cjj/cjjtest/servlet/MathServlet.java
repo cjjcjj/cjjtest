5fadd31f1ff587a8a8206f62f4fa557516e03551
cjjcjj <caojianjun0731@163.com>
2015-05-15 00:21:10 +0800

    @cjj

--- src/main/java/com/chuangwai/cjj/cjjtest/servlet/MathServlet.java
+++ src/main/java/com/chuangwai/cjj/cjjtest/servlet/MathServlet.java
@@ -3,13 +3,15 @@ package com.chuangwai.cjj.cjjtest.servlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by xingshi on 15/5/14.
  */
 public class MathServlet extends HttpServlet{
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
         response.setCharacterEncoding("utf-8");
         String a = request.getParameter("a");
         String b = request.getParameter("b");
