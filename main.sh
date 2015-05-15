50c429423ed6d6e1b4f8b7d2738cd615b59c50c4
cjjcjj <caojianjun0731@163.com>
2015-05-15 09:08:05 +0800

    @cjj

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean install
-java -Xms10m -Xmx10m -Xmn2m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -cp target/cjjtest-1.0-SNAPSHOT.jar:$(for i in target/lib/*.jar ; do echo -n $i: ; done) com.chuangwai.cjj.cjjtest.Main
+java -Xms30m -Xmx30m -Xmn2m -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -cp target/cjjtest-1.0-SNAPSHOT.jar:$(for i in target/lib/*.jar ; do echo -n $i: ; done) com.chuangwai.cjj.cjjtest.Main
